008c2c7362ca8b23534ea5eac48143dba44d3732
evelinaiv <111464715+evelinaiv@users.noreply.github.com>
2025-09-30 13:30:29 +0300

    Delete run_autotrain.sh

--- run_autotrain.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-# run the auto-trainer using your venv's Python
-cd /Users/evelinaivanova/sarcasm-api || exit 1
-exec /Users/evelinaivanova/sarcasm-api/.venv/bin/python /Users/evelinaivanova/sarcasm-api/autotrain.py
