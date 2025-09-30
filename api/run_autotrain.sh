6ab382c8c4bc73297143b79c3c496b958ac544b1
evelinaiv <111464715+evelinaiv@users.noreply.github.com>
2025-09-30 13:42:29 +0300

    Add files via upload

---
+++ api/run_autotrain.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# run the auto-trainer using your venv's Python
+cd /Users/evelinaivanova/sarcasm-api || exit 1
+exec /Users/evelinaivanova/sarcasm-api/.venv/bin/python /Users/evelinaivanova/sarcasm-api/autotrain.py
